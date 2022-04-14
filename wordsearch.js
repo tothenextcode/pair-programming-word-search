93f7415da05a7017a80afdf94f79412293fb3360
JC Flores <tothenextcode@gmail.com>
2022-04-14 04:19:09 +0000

    Implemented joinVertically and added test case for testing vertical search. Fixed bug that returned undefined instead of false if not match is found

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,31 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+  const verticalJoin = joinVertically(letters).map((ls) => ls.join(""));
+  
+  for (l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  
+  for (l of verticalJoin) {
+    console.log(l);
+    if (l.includes(word)) return true;
+  }
 
-module.exports = wordSearch
+  return false;
+};
+  
+const joinVertically = (letters) => {
+  const newArr = [];
+
+  for (let row = 0; row < letters[0].length; row++) {
+    newArr.push([]);
+
+    for (let col = 0; col < letters.length; col++) {
+      newArr[row].push(letters[col][row]);
+    }
+  }
+
+    return newArr;
+};
+  
+module.exports = wordSearch;
